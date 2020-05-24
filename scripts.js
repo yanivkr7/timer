88e40a2600e4751bfce3aa18e3db7972d1d25afe
yanivkr7 <yanivkr7@gmail.com>
2020-05-24 21:26:13 +0300

    Initial

---
+++ scripts.js
@@ -0,0 +1,103 @@
+let timerObj = {
+	minutes: 0,
+	seconds: 0,
+	timerId: 0
+};
+
+function soundAlarm() {
+	let amount = 3;
+	let audio = new Audio('Timer_Sound_Effect.mp3');
+
+	function playSound() {
+		audio.pause();
+		audio.currentTime = 0;
+		audio.play();
+	}
+
+	for (let i = 0; i < amount; i++) {
+		setTimeout(playSound, 1200 * i);
+	}
+}
+
+function updateValue(key, value) {
+	value = value < 0 ? 0 : value;
+	value = value > 59 ? 59 : value;
+
+	if (key === 'seconds') {
+		if (value < 10) {
+			value = '0' + value;
+		}
+	}
+
+	$('#' + key).html(value || 0);
+	timerObj[key] = value;
+
+	//console.log('minutes =' + timerObj.minutes, 'seconds = ' + timerObj.seconds);
+}
+
+(function detectChanges(key) {
+	//console.log('Detect Changes');
+
+	let input = '#' + key + '-input';
+
+	$(input).change(function() {
+		updateValue(key, $(input).val());
+	});
+
+	$(input).keyup(function() {
+		updateValue(key, $(input).val());
+	});
+	return arguments.callee;
+})('minutes')('seconds');
+
+function startTimer() {
+	buttonManager([ 'start', false ], [ 'pause', true ], [ 'stop', true ]);
+	freezeInputs();
+
+	timerObj.timerId = setInterval(function() {
+		timerObj.seconds--;
+		if (timerObj.seconds < 0) {
+			if (timerObj.minutes === 0) {
+				soundAlarm();
+				return stopTimer();
+			}
+			timerObj.seconds = 59;
+			timerObj.minutes--;
+		}
+
+		updateValue('minutes', timerObj.minutes);
+		updateValue('seconds', timerObj.seconds);
+	}, 1000);
+}
+function stopTimer() {
+	clearInterval(timerObj.timerId);
+	updateValue('minutes', $('#minutes-input').val());
+	updateValue('seconds', $('#seconds-input').val());
+	buttonManager([ 'start', true ], [ 'pause', false ], [ 'stop', false ]);
+	unFreezeInputs();
+}
+function pauseTimer() {
+	buttonManager([ 'start', true ], [ 'pause', false ], [ 'stop', true ]);
+	clearInterval(timerObj.timerId);
+}
+
+function buttonManager(...buttonsArray) {
+	buttonsArray.forEach((button) => {
+		let buttonId = '#' + button[0] + '-button';
+		if (button[1]) {
+			$(buttonId).removeAttr('disabled');
+		} else {
+			$(buttonId).attr('disabled', 'disabled');
+		}
+	});
+}
+
+function freezeInputs() {
+	$('#minutes-input').attr('disabled', 'disabled');
+	$('#seconds-input').attr('disabled', 'disabled');
+}
+
+function unFreezeInputs() {
+	$('#minutes-input').removeAttr('disabled');
+	$('#seconds-input').removeAttr('disabled');
+}
